a8aec771460b14cf76793bcf1f2357af954d61df
jorgev10 <100972047+jorgev10@users.noreply.github.com>
2022-11-01 11:46:47 -0600

    Update 2

--- er/Program.cs
+++ er/Program.cs
@@ -1,5 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Esto es un  prueba");
-Console.WriteLine("esto es una segunda preuba");
